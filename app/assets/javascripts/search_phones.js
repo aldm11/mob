ed03c23c51ed89dcbdf841c6a5c55e0de54be4bc
aldm11 <amer_king88@hotmail.com>
2013-05-21 22:05:55 +0200

    Finishing module for search/get using AJAX call and switching search phones to use this module

--- app/assets/javascripts/search_phones.js
+++ app/assets/javascripts/search_phones.js
@@ -1,24 +1,25 @@
 $(document).ready(function(){
-	var search_form = $("#main_search_form");
-	var min_price = 50;
-	var max_price = 1000;
+	var min_price = 50,
+	    max_price = 1000;
 	
-	var from = 0;
-	var size = 16;
+	var search_term = "", 
+	    brand = "", 
+	    price_from = min_price, 
+	    price_to = max_price, 
+	    sort_by = "date",
+	    from = 0,
+	    size = 16;
 	
 	$(".price-from-text").html(min_price);
 	$(".price-to-text").html(max_price);
-	$("#price_from").val(min_price);
-	$("#price_to").val(max_price);
-	$("#sort_by").val("date");
 	
 	var get_params =  function(){
 	   var params = {
-			"search_term" : $("#search_term").val(),
-			"sort_by" : $("#sort_by").val(),
-			"brand" : $("#brand").val(),
-			"price_from" : $("#price_from").val(),
-			"price_to" : $("#price_to").val(),
+			"search_term" : search_term,
+			"sort_by" : sort_by,
+			"brand" : brand,
+			"price_from" : price_from,
+			"price_to" : price_to,
 			"from" : from,
 			"size" : size
 		}
@@ -33,8 +34,8 @@ $(document).ready(function(){
 		slide: function( event, ui ) {
 			$(".price-from-text").html(ui.values[0]);
 			$(".price-to-text").html(ui.values[1]);
-			$("#price_from").val(ui.values[0]);
-			$("#price_to").val(ui.values[1]);
+			price_from = ui.values[0];
+			price_to = ui.values[1];
 		}
 	});
 	
@@ -49,18 +50,15 @@ $(document).ready(function(){
 	});
 	
 	$("#search_term").live("keyup", function(){
-		// $("#sort_by").val("relevance");
-		// from = 0;
-		// size = 16;
-		// submit_search_form();
-		
-		$("#sort_by").val("relevance");
+		sort_by = "relevance";
+		from = 0;
+		size = 16;
 		search("phones", get_params())
 	});
 	
 	$(".main-brands-list li a").live("click", function(){
-		$("#brand").val($(this).html());
-		$("#search_term").val("");
+		brand = $(this).html();
+		search_term = "";
 		search("phones", get_params());
 	});
 	
@@ -71,7 +69,7 @@ $(document).ready(function(){
 	$(".sort-phones-by a").live("click", function(){
 		$(".sort-phones-by a").removeClass("disabled");
 		$(this).addClass("disabled");
-		$("#sort_by").val($(this).attr("id"));
+		sort_by = $(this).attr("id");
 		
 		search("phones", get_params());
 	});
