448e9dd7ab7c7a128c92cc5b61150ed89611af85
aldm11 <amer_king88@hotmail.com>
2013-07-27 18:40:46 +0200

    Working on chat UI and backend

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,16 +3,17 @@ module ApplicationHelper
     Devise.mappings[:account]
   end
   
+  DEFAULT_LOGO = "/images/no_image.jpg"
   def current_account_json
     rolable = current_account.rolable
     account = {
-      :account_id => current_account.id.to_s, 
-      :username => current_account.username, 
-      :name => rolable.name, 
-      :image => rolable.logo.exists? ? rolable.logo.url : DEFAULT_LOGO, 
-      :email => current_account.email
+      "account_id" => current_account.id.to_s, 
+      "username" => current_account.username, 
+      "name" => rolable.name, 
+      "image" => rolable.logo.exists? ? rolable.logo.url : DEFAULT_LOGO, 
+      "email" => current_account.email
     }
-    account.to_json
+    account
   end
   
 end
