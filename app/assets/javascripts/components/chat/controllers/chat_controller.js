2d434e0a4ce7077f5960c374cc18ce5dae763f54
aldm11 <amer_king88@hotmail.com>
2013-11-15 21:24:32 +0100

    Finishing chat service and moving logic from controller to service

--- app/assets/javascripts/components/chat/controllers/chat_controller.js
+++ app/assets/javascripts/components/chat/controllers/chat_controller.js
@@ -2,249 +2,184 @@ var chat_app = angular.module("Chat");
 
 chat_app.controller("ChatController", ["$scope", "$timeout", "$http", "ChatService", function($scope, $timeout, $http, ChatService){
 	$scope.PROPERTIES = ["account_id", "username", "image", "name", "email"];
-		
-	//contacts should have size in bytes 130 * number_of_accounts so it should't be overhead for browser, check this out'
 	$scope.contacts = {};
 	$scope.conversations = {};
 	$scope.show_chat = true;
+	$scope.enabled = Configuration.enabled;
+	$scope.available = false;
+	//contacts should have size in bytes 130 * number_of_accounts so it should't be overhead for browser, check this out'
 	
-	$scope.enabled = $http.get("/api/V1/chat/config").then(function(response){
-		return response.data["enabled"];
-	}, function(error){
-		return false;
-	});
-	
-	$scope.user_logged_in = false;
-	
-	var user = $("#user").html();
-	
-	$scope.account = user ? JSON.parse(user) : user;
-	$scope.socket = null;
-	$scope.available = true;
-	
-	
-	
-	// ChatService.enabled().then(function(res){
-		// alert(JSON.stringify(res));
-	// });
-	ChatService.checkAndInit($scope.account);
-	
-	
-	// try {
-		// $scope.socket = io.connect("http://localhost:4000");
-		// $scope.available = true;
-	// } catch(err){
-		// console.log("Warning : Can't connect to chat server");
-		// $scope.available = false;
-	// }
-	
-	var logged_out_user =  $("#logged_out_user").html();
-	$scope.logged_out_user = logged_out_user ? JSON.parse(logged_out_user) : logged_out_user;
-	
-	if($scope.logged_out_user){
-		$scope.socket.emit("logout", $scope.logged_out_user.account_id);
-	}
-	
-	$scope.$on("user_logged_in", function(event, account){
-		$scope.contacts[account.account_id] = account;
-		
-		if($scope.conversations[account.account_id] !== undefined){
-			$scope.conversations[account.account_id].offline = false;
-		}
-		$scope.$apply();
-	});
-	
-	$scope.$on("message", function(event, message){
-		console.log("dosla");
-		console.log(message);
-		var sender_id = message.sender_id;
-		notifyMessageArrived(sender_id);
-		$scope.conversations[sender_id].messages.push({"sender" : $scope.contacts[sender_id].name, "content" : message.content});
-		$scope.$apply();
-	});
-	
-	$scope.$on("logout", function(event, account_id){
-		$scope.conversations[account_id].offline = true
-		delete $scope.contacts[account_id];
-		$scope.$apply();
-	});
-	
-	$scope.index = function(){
-		if($scope.account){
-			$scope.user_logged_in = true;
-			
-			// console.log("Logging in to server");
-			// $scope.socket.emit("login", $scope.account);
-			
-			// $scope.socket.on("login_complete", function(){
-				// $scope.socket.emit("onlineList", $scope.account.account_id);
-// 				
-				// $scope.socket.on("onlineListReply", function(accounts){
-					// console.log(accounts.length);
-					// updateContacts(accounts);
-					// console.log($scope.contacts);
-// 					
-					// $scope.socket.emit("getConversations", $scope.account.account_id);
-					// $scope.socket.on("conversations", function(server_conversations){
-						// updateConversations(server_conversations);
-						// console.log($scope.conversations);
-// 
-					// });
-				// });
-			// });
-			
-			ChatService.getOnlineList($scope.account).then(function(accounts){
-				updateContacts(accounts);
+	if(Configuration.enabled){			
+		var user = $("#user").html();
+		var logged_out_user =  $("#logged_out_user").html();
+		var chat_service = null;
 				
-				ChatService.getConversations($scope.account).then(function(conversations){
-					updateConversations(conversations);
-				}, function(err){			
+		$scope.account = user ? JSON.parse(user) : user;
+		
+		if(logged_out_user){
+			logged_out_user = JSON.parse(logged_out_user);
+			try{
+				new ChatService(logged_out_user).logout();
+			}
+			catch(err) {}
+		}
+		else {
+			try{
+				chat_service = new ChatService($scope.account);
+				$scope.available = true;
+			} catch(err){}
+		}
+		
+		// Event callbacks: message arrived, contact online and contact left
+		$scope.$on("user_logged_in", function(event, account){
+			$scope.contacts[account.account_id] = account;
+			
+			if($scope.conversations[account.account_id] !== undefined){
+				$scope.conversations[account.account_id].offline = false;
+			}
+			$scope.$apply();
+		});
+		
+		$scope.$on("message", function(event, message){
+			console.log("dosla");
+			console.log(message);
+			var sender_id = message.sender_id;
+			notifyMessageArrived(sender_id);
+			$scope.conversations[sender_id].messages.push({"sender" : $scope.contacts[sender_id].name, "content" : message.content});
+			$scope.$apply();
+		});
+		
+		$scope.$on("logout", function(event, account_id){
+			$scope.conversations[account_id].offline = true
+			delete $scope.contacts[account_id];
+			$scope.$apply();
+		});
+		
+		$scope.index = function(){
+			if(chat_service){
+				$scope.user_logged_in = true;
+	
+				chat_service.getOnlineList().then(function(accounts){
+					updateContacts(accounts);
+					
+					chat_service.getConversations().then(function(conversations){
+						updateConversations(conversations);
+					}, function(err){			
+					});
+				}, function(err){
+					
 				});
-			}, function(err){
+			}
+		};
+		
+		var CONVERSATION_WIDTH = 182;
+		$scope.toggleConversation = function(contact_id){
+			toggleConversation(contact_id);
+		};
+		
+		$scope.sendMessage = function(contact_id){
+			var message = $scope.conversations[contact_id].new_message;
+			
+			try {
+				chat_service.sendMessage(contact_id, message);		
+				$scope.conversations[contact_id].messages.push({"sender" : $scope.account.name, "content" : message});
+				$scope.$apply();
+			}
+			catch(err){
+				console.log("Error: " + err);
+			}
+		};
+		
+		$scope.showNoContacts = function(){
+			return angular.equals({}, $scope.contacts, {}) && $scope.available;
+		};
+		
+		$scope.conversationDisabled = function(account_id){
+			$scope.conversations[account_id].offline ? true : false;
+		};
+		
+		function notifyMessageArrived(contact_id){
+			var old_chat_width = $(".chat-content").width();
+			var new_chat_width = old_chat_width;
+			if($scope.conversations[contact_id] === undefined){
+				$scope.conversations[contact_id] = {};
+				$scope.conversations[contact_id]["details"] = $scope.contacts[contact_id];
+				$scope.conversations[contact_id]["new_message"] = "";
+				$scope.conversations[contact_id]["messages"] = [];
 				
-			});
-			
-			
-			// $scope.socket.emit("getConversations", $scope.account.account_id);
-			// $scope.socket.on("conversations", function(server_conversations){
-				// updateConversations(server_conversations);
-				// console.log($scope.conversations);
-// 
-			// });
-			
-			// $scope.socket.on("user_logged_in", function(account){
-				// $scope.$apply(function(){
-					// if($scope.contacts[account.account_id] ===  undefined){
-						// for(var i = 0; i < $scope.PROPERTIES.length; i++){
-							// $scope.contacts[account.account_id] = account;
-						// }
-					// }
-// 					
-					// if($scope.conversations[account.account_id] !== undefined){
-						// $scope.conversations[account.account_id].offline = false;
-					// }
-				// });					
-			// });
-// 			
-			// $scope.socket.on("message", function(message){
-				// $scope.$apply(function(){
-					// var sender_id = message.sender_id;
-					// notifyMessageArrived(sender_id);
-					// $scope.conversations[sender_id].messages.push({"sender" : $scope.contacts[sender_id].name, "content" : message.content});
-				// });
-			// });
-// 			
-			// $scope.socket.on("logout", function(account_id){
-				// $scope.$apply(function(){
-					// $scope.conversations[account_id].offline = true
-					// delete $scope.contacts[account_id];
-				// });
-			// })
-		}
-	};
-	
-	var CONVERSATION_WIDTH = 182;
-	$scope.toggleConversation = function(contact_id){
-		toggleConversation(contact_id);
-	};
-	
-	$scope.sendMessage = function(contact_id){
-		var message = $scope.conversations[contact_id].new_message;
-		
-		ChatService.sendMessage($scope.account.account_id, contact_id, message);		
-		$scope.conversations[contact_id].messages.push({"sender" : $scope.account.name, "content" : message});
-		$scope.$apply()
-		
-	};
-	
-	$scope.showNoContacts = function(){
-		return angular.equals({}, $scope.contacts, {}) && $scope.available;
-	};
-	
-	$scope.conversationDisabled = function(account_id){
-		$scope.conversations[account_id].offline ? true : false;
-	};
-	
-	function notifyMessageArrived(contact_id){
-		var old_chat_width = $(".chat-content").width();
-		var new_chat_width = old_chat_width;
-		if($scope.conversations[contact_id] === undefined){
-			$scope.conversations[contact_id] = {};
-			$scope.conversations[contact_id]["details"] = $scope.contacts[contact_id];
-			$scope.conversations[contact_id]["new_message"] = "";
-			$scope.conversations[contact_id]["messages"] = [];
-			
-			$scope.conversations[contact_id]["show"] = true;
-			
-			new_chat_width += CONVERSATION_WIDTH;
-		}
-		else{
-			if(!$scope.conversations[contact_id].show){
+				$scope.conversations[contact_id]["show"] = true;
+				
 				new_chat_width += CONVERSATION_WIDTH;
 			}
-
-			$scope.conversations[contact_id].show = true;
-		}				
-		$(".chat-content").width(new_chat_width);
-	}
-	
-	function toggleConversation(contact_id){
-		var old_chat_width = $(".chat-content").width();
-		var new_chat_width = old_chat_width;
-		if($scope.conversations[contact_id] === undefined){
-			$scope.conversations[contact_id] = {};
-			$scope.conversations[contact_id].details = $scope.contacts[contact_id];
-			$scope.conversations[contact_id].new_message = "";
-			$scope.conversations[contact_id].messages = [];
-			$scope.conversations[contact_id].started = (new Date()).getTime();
-							
-			$scope.conversations[contact_id].show = true;
-			
-			new_chat_width += CONVERSATION_WIDTH;
-		}
-		else{
-			$scope.conversations[contact_id].show = !$scope.conversations[contact_id].show;
-			
-			$scope.conversations[contact_id].show ? new_chat_width += CONVERSATION_WIDTH : new_chat_width -= CONVERSATION_WIDTH;
-		}
-		$(".chat-content").width(new_chat_width);
-	}
-	
-	function updateContacts(accounts){
-		var online_accounts = {};
-		for(var i = 0; i < accounts.length; i++){
-			var account = accounts[i];
-			if($scope.contacts[account.account_id] === undefined){
-				$scope.contacts[account.account_id] = account;
-			}
 			else{
-				for(var i = 0; i < $scope.PROPERTIES.length; i++){
-					$scope.contacts[account.account_id][$scope.PROPERTIES[i]] = account[$scope.PROPERTIES[i]];
+				if(!$scope.conversations[contact_id].show){
+					new_chat_width += CONVERSATION_WIDTH;
 				}
-			}
-			online_accounts[account.account_id] = 1;
+	
+				$scope.conversations[contact_id].show = true;
+			}				
+			$(".chat-content").width(new_chat_width);
 		}
 		
-		for(var account_id in $scope.contacts){
-			if(online_accounts[account_id] === undefined){
-				delete $scope.contacts[account_id];
+		function toggleConversation(contact_id){
+			var old_chat_width = $(".chat-content").width();
+			var new_chat_width = old_chat_width;
+			if($scope.conversations[contact_id] === undefined){
+				$scope.conversations[contact_id] = {};
+				$scope.conversations[contact_id].details = $scope.contacts[contact_id];
+				$scope.conversations[contact_id].new_message = "";
+				$scope.conversations[contact_id].messages = [];
+				$scope.conversations[contact_id].started = (new Date()).getTime();
+								
+				$scope.conversations[contact_id].show = true;
+				
+				new_chat_width += CONVERSATION_WIDTH;
+			}
+			else{
+				$scope.conversations[contact_id].show = !$scope.conversations[contact_id].show;
+				
+				$scope.conversations[contact_id].show ? new_chat_width += CONVERSATION_WIDTH : new_chat_width -= CONVERSATION_WIDTH;
+			}
+			$(".chat-content").width(new_chat_width);
+		}
+		
+		function updateContacts(accounts){
+			var online_accounts = {};
+			for(var i = 0; i < accounts.length; i++){
+				var account = accounts[i];
+				if($scope.contacts[account.account_id] === undefined){
+					$scope.contacts[account.account_id] = account;
+				}
+				else{
+					for(var i = 0; i < $scope.PROPERTIES.length; i++){
+						$scope.contacts[account.account_id][$scope.PROPERTIES[i]] = account[$scope.PROPERTIES[i]];
+					}
+				}
+				online_accounts[account.account_id] = 1;
+			}
+			
+			for(var account_id in $scope.contacts){
+				if(online_accounts[account_id] === undefined){
+					delete $scope.contacts[account_id];
+				}
+			}
+		}
+		
+		function updateConversations(server_conversations){
+			for(var account_id in server_conversations){
+				if($scope.contacts[account_id]){
+					toggleConversation(account_id);
+					
+					for(var i = 0; i < server_conversations[account_id].messages.length; i++){
+						var sender_id = server_conversations[account_id].messages[i].sender_id;
+						var sender_name = sender_id === $scope.account.account_id ? $scope.account.name : $scope.contacts[sender_id].name;
+						var mess = {"sender" : sender_name, "content" : server_conversations[account_id].messages[i].content};									
+						
+						$scope.conversations[account_id].messages.push(mess);									
+					}
+				}		
 			}
 		}
 	}
 	
-	function updateConversations(server_conversations){
-		for(var account_id in server_conversations){
-			if($scope.contacts[account_id]){
-				toggleConversation(account_id);
-				
-				for(var i = 0; i < server_conversations[account_id].messages.length; i++){
-					var sender_id = server_conversations[account_id].messages[i].sender_id;
-					var sender_name = sender_id === $scope.account.account_id ? $scope.account.name : $scope.contacts[sender_id].name;
-					var mess = {"sender" : sender_name, "content" : server_conversations[account_id].messages[i].content};									
-					
-					$scope.conversations[account_id].messages.push(mess);									
-				}
-			}		
-		}
-	}
 }]);
--- app/assets/javascripts/components/chat/services/chat_service.js
+++ app/assets/javascripts/components/chat/services/chat_service.js
@@ -1,96 +1,88 @@
 var char_app = angular.module("Chat");
 chat_app.factory("ChatService", ["$http", "$log", "$q", "$rootScope", function($http, $log, $q, $rootScope){
 	
-	var chat_service = {};
-	var socket = null;
-	var enabled = false;
-	var account = null;
+	return function(user_account){
+		var socket = null;
+		var enabled = false;
+		var account = user_account;
 		
-	chat_service.checkAndInit = function(acc){
-		account = acc;
-		if(socket !== null) return true;
+		if(!angular.isObject(user_account)) throw "Invalid user";
+				
+		socket = io.connect("http://localhost:4000");
 		
-		try {
-			socket = io.connect("http://localhost:4000");
-			
-			socket.on("user_logged_in", function(account){
-				$rootScope.$broadcast("user_logged_in", account);
-			});
-			
-			socket.on("message", function(message){				
-				$rootScope.$broadcast("message", message);
-			});
-			
-			socket.on("logout", function(account_id){
-				$rootScope.$broadcast("logout", account_id);
-			})
-			
-			return true;
-		} catch(err){
-			console.log("Warning : Can't connect to chat server");
-			return false;
-		}
-	};
-	
-	chat_service.enabled = function(){
-		return $http.get("/api/V1/chat/config").then(function(response){
-			return response.data["enabled"];
-		}, function(error){
-			return false;
+		socket.on("user_logged_in", function(account){
+			$rootScope.$broadcast("user_logged_in", account);
 		});
-	};
-	
-	chat_service.login = function(account){
-		var deferred = $q.defer();
-		if(socket){
-			console.log("Logging in to server");
-			socket.emit("login", account);
+		
+		socket.on("message", function(message){				
+			$rootScope.$broadcast("message", message);
+		});
+		
+		socket.on("logout", function(account_id){
+			$rootScope.$broadcast("logout", account_id);
+		});	
+		
+		var chat_service = {};
+		
+		chat_service.login = function(){
+			var deferred = $q.defer();
 			
-			socket.on("login_complete", function(){
-				deferred.resolve(true);
+			if(socket){
+				console.log("Logging in to server");
+				socket.emit("login", account);
+				
+				socket.on("login_complete", function(){
+					deferred.resolve(true);
+					$rootScope.$apply();
+				});
+			}
+			else {
+				deferred.reject();
+			}
+			return deferred.promise;
+		};
+		
+		chat_service.getOnlineList = function(){
+			var deferred = $q.defer();
+			
+			chat_service.login().then(function(acc){
+				socket.emit("onlineList", account.account_id);
+				
+				socket.on("onlineListReply", function(accounts){
+					console.log("Chat service contacts");
+					deferred.resolve(accounts);
+					$rootScope.$apply();
+				});		
+			}, function(err){
+				deferred.reject();
+			});
+			
+			return deferred.promise;
+		}
+		
+		chat_service.getConversations = function(){
+			var deferred = $q.defer();
+			
+			socket.emit("getConversations", account.account_id);
+			socket.on("conversations", function(conversations){
+				console.log("Chat service conversations:");
+				console.log(conversations);
+				deferred.resolve(conversations);
 				$rootScope.$apply();
 			});
-		}
-		else {
-			deferred.reject();
-		}
-		return deferred.promise;
-	};
-	
-	chat_service.getOnlineList = function(account){
-		var deferred = $q.defer();
-		
-		chat_service.login(account).then(function(acc){
-			socket.emit("onlineList", account.account_id);
 			
-			socket.on("onlineListReply", function(accounts){
-				console.log("accounts");
-				deferred.resolve(accounts);
-				$rootScope.$apply();
-			});		
-		}, function(err){
-			deferred.reject();
-		});
+			return deferred.promise;
+		}
 		
-		return deferred.promise;
-	}
-	
-	chat_service.getConversations = function(account){
-		var deferred = $q.defer();
+		chat_service.sendMessage = function(to, message){
+			console.log({"sender_id" : account.account_id, "receiver_id" : to, "content" : message});		
+			socket.emit("clientMessage", {"sender_id" : account.account_id, "receiver_id" : to, "content" : message});	
+		}
 		
-		socket.emit("getConversations", account.account_id);
-		socket.on("conversations", function(conversations){
-			console.log(conversations);
-			deferred.resolve(conversations);
-			$rootScope.$apply();
-		});
+		chat_service.logout = function(){			
+			socket.emit("logout", account.account_id);
+		}
 		
-		return deferred.promise;
-	}
-	
-	chat_service.sendMessage = function(from, to, message){
-		socket.emit("clientMessage", {"sender_id" : from, "receiver_id" : to, "content" : message.content});	
-	}
-	
-	return chat_service;
+		return chat_service;
+	};
 }]);
