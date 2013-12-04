d14c8b7ef8d656dfb29f5c5c04c2706a5b66a16a
aldm11 <amer_king88@hotmail.com>
2013-12-04 22:04:07 +0100

    Finished reorganizing home & messages css & js

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -5,8 +5,9 @@ jQuery(function($){
 	// $("#login_form").bind("ajax:beforeSend", function(event, data, status, xhr) {		
 	// });	
 // 	
-	// $("#login_form").bind("ajax:success", function(event, data, status, xhr) {
-    // });
+	$("#login_form").bind("ajax:success", function(event, data, status, xhr) {
+		window.location = "/"
+    });
     
     $("#login_form").bind("ajax:error", function(xhr, status, error) {
 		$("<div class='alert-error'>Pogrešni login podaci</div>").appendTo($("#login_form"));
